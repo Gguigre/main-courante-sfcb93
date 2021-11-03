24885043bf65fbf63cc19492acc0c8b8dc1257d8
hduprat <hugod@bam.tech>
2021-11-03 14:17:52 +0100

    :sparkles: (StyledText) render component with testing-library

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -1,11 +1,11 @@
 /* eslint-disable react-native/no-raw-text */
 import * as React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 
 import { MonoText } from '../StyledText';
 
 it(`renders correctly`, () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  const tree = render(<MonoText>Snapshot test!</MonoText>);
 
   expect(tree).toMatchSnapshot();
 });
